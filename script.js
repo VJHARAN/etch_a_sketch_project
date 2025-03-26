14bb169b145bcdac3e3740b86adcfad62f558f3b
VJHARAN <haranvj1997@gmail.com>
2025-03-26 11:29:27 +0530

    Add hover effect to change color of squares

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ const container=document.querySelector('.container');
 for (let i=1;i<=16;i++){
     for(let j=1;j<=16;j++){
         let div=document.createElement('div');
-         
+        div.classList.add('square');
         container.appendChild(div);
     }
      
 }
+
+const squareDivs=document.querySelectorAll('.square');
+console.log(squareDivs);
+squareDivs.forEach((divs)=>{
+    divs.addEventListener('mouseover',()=>{
+        divs.style.backgroundColor='#000';
+    })
+})
