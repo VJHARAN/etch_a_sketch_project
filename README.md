af185e2c3159fd7072589ee2c9f7f0e8e3c474d4
VJHARAN <haranvj1997@gmail.com>
2025-03-26 21:45:21 +0530

    Add RGB colors & update readme file
    Randomized the squares’ RGB values with each interaction.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Etch_a_Sketch_project
-Aims to build a browser version of something between a sketchpad and an Etch-A-Sketch.
+<h1>SKETCHPAD project</h1>
+
+<h3>Description</h3>
+<p>Built a browser version of a Sketchpad .</p>
+
+<h3><a href="https://vjharan.github.io/etch_a_sketch_project/">view project</a></h3>
+
+<h3>Project Screenshot</h3>
+<img src="./images/black-grid.png" alt="screenshot1" width="80%">
+<img src="./images/rgb-grid.png" alt="screenshot2" width="80%">
+ 
+
+<h3>Features</h3>
+<ul>
+    <li>Default grid size : 16x16 squares</li>
+    <li>Option to set custom sketchpad size in the range 1-100.</li>
+    <li>Option to use RGB colored grid or Black colored Grid</li>
+    <li>Option to clear current grid</li>
+</ul>
+
+<h3>How to use the Sketchpad:</h3>
+<ul>
+    <li>Users can hover the mouse over the small squares to create desired patterns on the sketchpad.</li>
+    <li>Click 'Change Sketchpad size' button to input custom size between 1 and 100.</li>
+    <li>Click on the adjacent button to toggle between 'RGB GRID' or 'BLACK GRID'.</li>
+    <li>Click on the 'Clear Grid' button to clear the pattern on the current grid</li>
+</ul>
+
+<h3>Technologies used</h3>
+<ul>
+    <li>HTML5</li>
+    <li>CSS</li>
+    <li>JavaScript</li>
+</ul>
--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 
-function createGrid(size=16){
+function createBlackGrid(size=16){
     const squareSize=100/size;
-     
-    console.log(squareSize);
     for (let i=1;i<=size;i++){
         for(let j=1;j<=size;j++){
             let div=document.createElement('div');
             div.classList.add('square');
             div.style.width=`${squareSize}%`;
             div.style.height=`${squareSize}%`;
-
             container.appendChild(div);
         } 
     }
@@ -21,9 +18,36 @@ function createGrid(size=16){
             divs.style.border='3px solid #27cc06';
         })
     })
-
-
 }
+
+
+function createRGBGrid(size=16){
+    const squareSize=100/size;
+    for (let i=1;i<=size;i++){
+        for(let j=1;j<=size;j++){
+            let div=document.createElement('div');
+            div.classList.add('square');
+            div.style.width=`${squareSize}%`;
+            div.style.height=`${squareSize}%`;
+
+            container.appendChild(div);
+        } 
+    }
+     const squareDivs=document.querySelectorAll('.square');
+     squareDivs.forEach((divs)=>{
+        divs.addEventListener('mouseover',()=>{
+            divs.style.backgroundColor=`rgb(${rgbGrid()},${rgbGrid()},${rgbGrid()})`;
+            divs.style.border='3px solid rgb(0, 0, 0)';
+        })
+    })
+}
+
+
+function rgbGrid(){
+   let rgbVal=Math.floor(Math.random()*256);
+   return rgbVal;
+}
+
 
 function removeGrid(){
     container.innerHTML='';
@@ -37,24 +61,60 @@ function clearGrid(){
         divs.style.border='1px solid #000';
      })
 }
+
 const container=document.querySelector('.container');
 const button=document.querySelector('button');
 button.addEventListener('click',()=>{
     const gridSize= prompt('Set squares per side for Sketchpad (1-100)');
-    if (gridSize>0 && gridSize<=100){
+    if (gridSize>0 && gridSize<=100 && flag==0){
         removeGrid();
-        createGrid(gridSize);
+        createBlackGrid(gridSize);
+    }
+    else if(gridSize>0 && gridSize<=100 && flag==1){
+        removeGrid();
+        createRGBGrid(gridSize);
+
     }
     else{
         alert("Input a value between 1-100 !");
     }
 })
+
+
 const clear=document.querySelector('.clear');
 clear.addEventListener('click',()=>{
     clearGrid();
 })
 
+const switchColor=document.querySelector('#rgb-button');
+switchColor.textContent="RGB GRID";
+switchColor.style.backgroundColor='#5805b0';
+switchColor.style.color='#FFF';
+switchColor.addEventListener('click',()=>{
+    
+    if (switchColor.getAttribute('class')=='rgb'){
+        flag=1;
+        switchColor.classList.toggle('rgb');
+        switchColor.classList.toggle('mono');
+        switchColor.textContent="BLACK GRID";
+        switchColor.style.backgroundColor='#000';
+        switchColor.style.color='#FFF';
+        removeGrid();
+        createRGBGrid();
+    }
+    else{
+        flag=0;
+        switchColor.textContent="RGB GRID";
+        switchColor.classList.toggle('mono');
+        switchColor.classList.toggle('rgb');
+        switchColor.style.backgroundColor='#5805b0';
+        switchColor.style.color='#FFF';
+        removeGrid();
+        createBlackGrid();   
+    }
+})
 
-createGrid();
+let flag=0;
+createBlackGrid();
 
 
